3ed9ea787638659e73253c2b766e1ee1279eee2e
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-03-07 19:47:43 -0500

    Add files via upload
    holders

---
+++ ViewHolder/ProductViewHolder.java
@@ -0,0 +1,38 @@
+package com.owen.netmall.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.owen.netmall.Interface.ItemClickListner;
+import com.owen.netmall.R;
+
+public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+
+{
+
+    public TextView tvProductName, tvProductDescription;
+    public ImageView ivProductImage;
+    public ItemClickListner listner;
+
+    public ProductViewHolder(View itemView)
+    {
+        super(itemView);
+
+        ivProductImage = (ImageView) itemView.findViewById(R.id.item_product_image);
+        tvProductName = (TextView) itemView.findViewById(R.id.item_product_name);
+        tvProductDescription = (TextView) itemView.findViewById(R.id.item_product_description);
+    }
+
+    public void setItemClickListner(ItemClickListner listner)
+    {
+        this.listner = listner;
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        listner.onClick(view ,getAdapterPosition(), false);
+    }
+}
